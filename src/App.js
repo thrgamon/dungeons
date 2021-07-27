1ad166e0bf4ddc734d442756a9c02b367cb94c92
Tom Gamon <tom@tomgamon.com>
2021-07-27 17:58:37 +1000

    Basic graph

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ForceGraph2D from 'react-force-graph-2d';
 
+const myData = {
+    "nodes": [
+        {
+          "id": "1",
+          "name": "name1",
+          "val": 1
+        },
+        {
+          "id": "2",
+          "name": "name2",
+          "val": 10
+        }
+    ],
+    "links": [
+        {
+            "source": "1",
+            "target": "2"
+        }
+    ]
+}
 function App() {
+  const [graphData, setGraphData] = useState(myData);
+  const [currentNode, setCurrentNode] = useState(myData["nodes"][0]);
+
+  const addNode = () => {
+    const {nodes, links} = graphData
+    const newNodes = nodes.slice();
+    const currentNodeId = currentNode.id
+
+    let newNode = {
+      id: currentNodeId
+    }
+
+    setCurrentNode(currentNode + 1)
+    setGraphData({ nodes: [...newNodes, newNode], links: links });
+  }
+
+  const updateNode = (node, value) => {
+    const {nodes, links} = graphData
+    let newNodes = nodes.slice();
+    const newLinks = links.slice();
+
+    const targetNodeIdx = newNodes.findIndex(el => el.id === node)
+    const targetNode = newNodes[targetNodeIdx]
+    newNodes.splice(targetNodeIdx, 1)
+    targetNode["cal"] = value
+    setGraphData({ nodes: [...newNodes, targetNode], links: links});
+  }
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const node = event.target.name;
+    updateNode(node, value)
+  }
+
+  const setNode = (node, event) => {
+    setCurrentNode(node)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <ForceGraph2D
+        graphData={graphData}
+        onNodeClick={setNode}
+      />
+      <div class="overlay">
+        <h2>{String(currentNode.id)}</h2>
+        <textarea name={String(currentNode.id)} onChange={handleChange} value={currentNode.cal || ""}/>
+      <button onClick={addNode}>
+        Add Node
+      </button>
+      </div>
     </div>
   );
 }
